7fd2e22014d33db7b487a58100e836494cfd923e
Ádám Horváth <horada1991@gmail.com>
2017-04-25 14:30:15 +0200

    FileLogger class' save log into file method
    file is saved into debug/logs/ folder (debug folder is where you can find the runnable exe file)

---
+++ ProjectManager2000/Util/FileLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager2000.Util
+{
+    class FileLogger
+    {
+        public string FileName { get; set; }
+        private const string LogPath = "logs/";
+
+        public FileLogger(string fileName)
+        {
+            FileName = fileName;
+            Directory.CreateDirectory("logs");
+
+            if (File.Exists(LogPath + FileName)) return;
+            var file = File.CreateText(LogPath + FileName);
+            file.Close();
+        }
+
+        public void SaveLog(string eventDescripion)
+        {
+            var file = File.AppendText(LogPath + FileName);
+            file.WriteLine(DateTime.Now + " " + eventDescripion);
+            file.Close();
+        }
+
+    }
+}
