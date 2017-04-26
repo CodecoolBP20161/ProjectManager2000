e2378076cda64b7a3744e080bbdedad8f734e37f
Ádám Horváth <horada1991@gmail.com>
2017-04-26 11:52:37 +0200

    Swapping between windows

--- ProjectManager2000/TestWindow.xaml.cs
+++ ProjectManager2000/TestWindow.xaml.cs
@@ -10,26 +10,29 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ProjectManager2000
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TestWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TestWindow : Window
     {
-        public MainWindow()
+        public TestWindow()
         {
             InitializeComponent();
         }
 
-        private void ToTestWindowBtn_Click(object sender, RoutedEventArgs e)
+        private void ToMainWindowBtn_Click(object sender, RoutedEventArgs e)
         {
-            TestWindow testWindow = new TestWindow();
-            testWindow.Show();
+            Application.Current.MainWindow.Show();
             this.Hide();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
         }
     }
 }
