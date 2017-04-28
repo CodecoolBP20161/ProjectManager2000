a430af30c9d19ecd07538f821984960fd6fb56fe
Rév Kálmán <revkalman@gmail.com>
2017-04-28 08:27:25 +0200

    próba

--- ProjectManager2000/MainWindow.xaml.cs
+++ ProjectManager2000/MainWindow.xaml.cs
@@ -21,14 +21,17 @@ namespace ProjectManager2000
     /// </summary>
     public partial class MainWindow : Window
     {
+        Controller.ProjectDAO projectDao = new Controller.ProjectDAO();
         public MainWindow()
         {
             InitializeComponent();
-            RenderLogTab();
+            RenderAssignTab();
+            //RenderLogTab();
         }
 
         private void RenderLogTab()
         {
+
             string[] logFiles = Directory.GetFiles("logs/").Select(System.IO.Path.GetFileName).ToArray();
             LogFilesDropDown.ItemsSource = logFiles;
             LogFilesDropDown.SelectedItem = LogFilesDropDown.Items[0];
@@ -36,6 +39,8 @@ namespace ProjectManager2000
             FileLogger fileLogger = new FileLogger(logFiles[0]);
             LogList.ItemsSource = fileLogger.GetLogs();
         }
+
+
 
         private void LogFilesDropDown_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -47,6 +52,54 @@ namespace ProjectManager2000
         private void LogList_LayoutUpdated(object sender, EventArgs e)
         {
             LogListColumn.Width = LogList.ActualWidth;
+        }
+
+        private void projektListAss_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            studentList.Items.Clear();
+            Model.Project current = (Model.Project) projektListAss.SelectedItem;
+            foreach (String participiant in current.Participants)
+            {
+                studentList.Items.Add(participiant);
+            }
+            descField.Items.Clear();
+            descField.Items.Add(current.Description);
+        }
+        private void RenderAssignTab()
+        {
+            List<Model.Project> projectList = projectDao.getAll();
+            foreach (Model.Project proj in projectList)
+            {
+                projektListAss.Items.Add(proj);
+            }
+        }
+
+        private void refreshPList_Click(object sender, RoutedEventArgs e)
+        {
+            projektListAss.Items.Clear();
+            List<Model.Project> projectList = projectDao.getAll();
+            foreach (Model.Project proj in projectList)
+            {
+                projektListAss.Items.Add(proj);
+            }
+        }
+
+        private void studentList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void applyBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void applyButton_Click(object sender, RoutedEventArgs e)
+        {
+            String newParticipiant =  applyBox.Text;
+            Model.Project current = (Model.Project)projektListAss.SelectedItem;
+            current.Participants.Add(newParticipiant);
+            projectDao.UpdateProject(current);
         }
     }
 }
